4a60cc31e0b9660dfd23d7f4c1edba9f010600dc
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-08-20 13:53:58 +0200

    IMPROVE BUTTON COLOR HANDLING

--- lib/frontend/css/config.php
+++ lib/frontend/css/config.php
@@ -13,8 +13,22 @@
 			$script->get_parent()->get_setting('font')->get_css_data('font-family'),
 			$script->get_parent()->get_setting('font_size')->get_css_data('font-size','','px'),
 			$script->get_parent()->get_setting('line_height')->get_css_data('line-height'),
-			$script->get_parent()->get_setting('text_color')->get_css_data(),
-			$script->get_parent()->get_setting('bg_color')->get_css_data('background-color'),
 			$script->get_parent()->get_setting('padding')->get_css_data('padding')
+		)
+	);
+
+	echo $_s->build_css(
+		is_admin() ? '.editor-styles-wrapper .wp-block-button:not(.is-style-outline) > .wp-block-button__link' : '.sv100_sv_content_wrapper article .wp-block-button:not(.is-style-outline) > .wp-block-button__link',
+		array_merge(
+			$script->get_parent()->get_setting('text_color')->get_css_data(),
+			$script->get_parent()->get_setting('bg_color')->get_css_data('background-color')
+		)
+	);
+
+	echo $_s->build_css(
+		is_admin() ? '.editor-styles-wrapper .wp-block-button.is-style-outline > .wp-block-button__link' : '.sv100_sv_content_wrapper article .wp-block-button.is-style-outline > .wp-block-button__link',
+		array_merge(
+			$script->get_parent()->get_setting('text_color')->get_css_data('background-color'),
+			$script->get_parent()->get_setting('bg_color')->get_css_data('color')
 		)
 	);
