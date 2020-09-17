31af2a767a4769a668fac994207412af11105080
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-09-17 20:56:23 +0200

    LOAD SETTINGS & SCRIPTS ONLY WHEN NEEDED

--- sv_block_button.php
+++ sv_block_button.php
@@ -15,8 +15,6 @@
 		public function init() {
 			$this->set_module_title( __( 'Block: Button', 'sv100' ) )
 				->set_module_desc( __( 'Settings for Gutenberg Block', 'sv100' ) )
-				->load_settings()
-				->register_scripts()
 				->set_section_title( $this->get_module_title() )
 				->set_section_desc( $this->get_module_desc() )
 				->set_section_type( 'settings' )
@@ -112,6 +110,10 @@
 		public function enqueue_scripts(): sv_block_button {
 			if(!$this->has_block_frontend('button')){
 				return $this;
+			}
+
+			if(!is_admin()){
+				$this->load_settings()->register_scripts();
 			}
 
 			foreach($this->get_scripts() as $script){
