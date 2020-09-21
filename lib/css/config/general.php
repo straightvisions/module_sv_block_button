b471bec3f106f7e426d189039398b06c74594dc2
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-09-21 19:35:34 +0200

    MIGRATE TO V5.100

--- lib/css/config/general.php
+++ lib/css/config/general.php
@@ -2,33 +2,33 @@
 	echo $_s->build_css(
 		is_admin() ? '.editor-styles-wrapper .wp-block-button' : '.sv100_sv_content_wrapper article .wp-block-button',
 		array_merge(
-			$script->get_parent()->get_setting('margin')->get_css_data(),
-			$script->get_parent()->get_setting('border')->get_css_data()
+			$module->get_setting('margin')->get_css_data(),
+			$module->get_setting('border')->get_css_data()
 		)
 	);
 
 	echo $_s->build_css(
 		is_admin() ? '.editor-styles-wrapper .wp-block-button > .wp-block-button__link' : '.sv100_sv_content_wrapper article .wp-block-button > .wp-block-button__link',
 		array_merge(
-			$script->get_parent()->get_setting('font')->get_css_data('font-family'),
-			$script->get_parent()->get_setting('font_size')->get_css_data('font-size','','px'),
-			$script->get_parent()->get_setting('line_height')->get_css_data('line-height'),
-			$script->get_parent()->get_setting('padding')->get_css_data('padding')
+			$module->get_setting('font')->get_css_data('font-family'),
+			$module->get_setting('font_size')->get_css_data('font-size','','px'),
+			$module->get_setting('line_height')->get_css_data('line-height'),
+			$module->get_setting('padding')->get_css_data('padding')
 		)
 	);
 
 	echo $_s->build_css(
 		is_admin() ? '.editor-styles-wrapper .wp-block-button:not(.is-style-outline) > .wp-block-button__link' : '.sv100_sv_content_wrapper article .wp-block-button:not(.is-style-outline) > .wp-block-button__link',
 		array_merge(
-			$script->get_parent()->get_setting('text_color')->get_css_data(),
-			$script->get_parent()->get_setting('bg_color')->get_css_data('background-color')
+			$module->get_setting('text_color')->get_css_data(),
+			$module->get_setting('bg_color')->get_css_data('background-color')
 		)
 	);
 
 	echo $_s->build_css(
 		is_admin() ? '.editor-styles-wrapper .wp-block-button.is-style-outline > .wp-block-button__link' : '.sv100_sv_content_wrapper article .wp-block-button.is-style-outline > .wp-block-button__link',
 		array_merge(
-			$script->get_parent()->get_setting('text_color')->get_css_data('background-color'),
-			$script->get_parent()->get_setting('bg_color')->get_css_data('color')
+			$module->get_setting('text_color')->get_css_data('background-color'),
+			$module->get_setting('bg_color')->get_css_data('color')
 		)
 	);
