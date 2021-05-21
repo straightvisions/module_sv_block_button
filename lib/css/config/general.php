f7c192c31966ce5b1c8fecca5b788317740f35d2
Matthias Bathke <matthias-bathke@straightvisions.com>
2021-05-21 18:52:03 +0200

    REBUILD CSS

--- lib/css/config/general.php
+++ lib/css/config/general.php
@@ -1,5 +1,110 @@
 <?php
-	/* Default: Spacing & Border */
+	/*
+	 * not outline
+	 * not hover
+	 * not active
+	 * not text color
+	 */
+	echo $_s->build_css(
+		is_admin() ?
+			'.editor-styles-wrapper .wp-block-button:not(.is-style-outline) .wp-block-button__link:not(:hover):not(:active):not(.has-text-color)' :
+			'.sv100_sv_content_wrapper article .wp-block-button:not(.is-style-outline) .wp-block-button__link:not(:hover):not(:active):not(.has-text-color)',
+		array_merge(
+			$module->get_setting('text_color')->get_css_data('color')
+		)
+	);
+
+	/*
+	 * not outline
+	 * not hover
+	 * not background
+	 */
+	echo $_s->build_css(
+		is_admin() ?
+			'.editor-styles-wrapper .wp-block-button:not(.is-style-outline) .wp-block-button__link:not(:hover):not(:active):not(.has-background)' :
+			'.sv100_sv_content_wrapper article .wp-block-button:not(.is-style-outline) .wp-block-button__link:not(:hover):not(:active):not(.has-background)',
+		array_merge(
+			$module->get_setting('bg_color')->get_css_data('background-color')
+		)
+	);
+
+	/*
+	 * has outline
+	 * not hover
+	 * not active
+	 * not text color
+	 */
+	echo $_s->build_css(
+		is_admin() ?
+			'.editor-styles-wrapper .wp-block-button.is-style-outline .wp-block-button__link:not(:hover):not(:active):not(.has-text-color)' :
+			'.sv100_sv_content_wrapper article .wp-block-button.is-style-outline .wp-block-button__link:not(:hover):not(:active):not(.has-text-color)',
+		array_merge(
+			$module->get_setting('text_color')->get_css_data('color')
+		)
+	);
+
+	/*
+	 * has outline
+	 * has hover
+	 * has active
+	 */
+	echo $_s->build_css(
+		is_admin() ?
+			'.editor-styles-wrapper .wp-block-button.is-style-outline .wp-block-button__link:hover, .editor-styles-wrapper .wp-block-button.is-style-outline .wp-block-button__link:active' :
+			'.sv100_sv_content_wrapper article .wp-block-button.is-style-outline .wp-block-button__link:hover, .sv100_sv_content_wrapper article .wp-block-button.is-style-outline .wp-block-button__link:active',
+		array_merge(
+			$module->get_setting('text_color')->get_css_data('background-color'),
+			$module->get_setting('bg_color')->get_css_data('color')
+		)
+	);
+
+	/*
+	 * has background
+	 * has outline
+	 * has hover
+	 * has active
+	 */
+	echo $_s->build_css(
+		is_admin() ?
+			'.editor-styles-wrapper .has-background .wp-block-button.is-style-outline .wp-block-button__link:hover, .editor-styles-wrapper .has-background .wp-block-button.is-style-outline .wp-block-button__link:active' :
+			'.sv100_sv_content_wrapper article .has-background .wp-block-button.is-style-outline .wp-block-button__link:hover, .sv100_sv_content_wrapper article .has-background .wp-block-button.is-style-outline .wp-block-button__link:active',
+		array_merge(
+			$module->get_setting('text_color')->get_css_data('color')
+		)
+	);
+
+	/*
+	 * has color
+	 * has outline
+	 * has hover
+	 * has active
+	 */
+
+	echo $_s->build_css(
+		is_admin() ?
+			'.editor-styles-wrapper .has-text-color .wp-block-button.is-style-outline .wp-block-button__link:hover, .editor-styles-wrapper .has-text-color .wp-block-button.is-style-outline .wp-block-button__link:active' :
+			'.sv100_sv_content_wrapper article .has-text-color .wp-block-button.is-style-outline .wp-block-button__link:hover, .sv100_sv_content_wrapper article .has-text-color .wp-block-button.is-style-outline .wp-block-button__link:active',
+		array_merge(
+			$module->get_setting('bg_color')->get_css_data('color')
+		)
+	);
+
+	/*
+	 * has color
+	 * has outline
+	 * has radius
+	 * has focus
+	 */
+	echo $_s->build_css(
+		is_admin() ?
+			'.editor-styles-wrapper .is-style-outline .wp-block-button__link[style*=radius]:focus, .editor-styles-wrapper .wp-block-button a.wp-block-button__link[style*=radius]:focus' :
+			'.sv100_sv_content_wrapper article .is-style-outline .wp-block-button__link[style*=radius]:focus, .sv100_sv_content_wrapper article .editor-styles-wrapper .wp-block-button a.wp-block-button__link[style*=radius]:focus',
+		array_merge(
+			$module->get_setting('bg_color')->get_css_data('outline-color')
+		)
+	);
+
+	// Spacing & Border
 	echo $_s->build_css(
 		is_admin() ? '.editor-styles-wrapper .wp-block-button' : '.sv100_sv_content_wrapper article .wp-block-button',
 		array_merge(
@@ -7,40 +112,15 @@
 		)
 	);
 	
-	/* Default: Sizing */
+	// Default: Sizing
 	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper .wp-block-button > .wp-block-button__link' : '.sv100_sv_content_wrapper article .wp-block-button > .wp-block-button__link',
+		is_admin() ? '.editor-styles-wrapper .wp-block-buttons .wp-block-button > .wp-block-button__link' : '.sv100_sv_content_wrapper article .wp-block-buttons .wp-block-button > .wp-block-button__link',
 		array_merge(
 			$module->get_setting('font')->get_css_data('font-family'),
 			$module->get_setting('font_size')->get_css_data('font-size','','px'),
 			$module->get_setting('line_height')->get_css_data('line-height'),
 			$module->get_setting('padding')->get_css_data('padding'),
 			$module->get_setting('border')->get_css_data()
-		)
-	);
-	
-	/* Filled: Text Color */
-	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper .wp-block-button:not(.is-style-outline) > .wp-block-button__link:not(.has-text-color)' : '.sv100_sv_content_wrapper article .wp-block-button:not(.is-style-outline) > .wp-block-button__link:not(.has-text-color)',
-		array_merge(
-			$module->get_setting('text_color')->get_css_data()
-		)
-	);
-	
-	/* Filled & Filled: Background Color */
-	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper .wp-block-button > .wp-block-button__link:not(.has-background)' : '.sv100_sv_content_wrapper article .wp-block-button > .wp-block-button__link:not(.has-background)',
-		array_merge(
-			$module->get_setting('bg_color')->get_css_data('background-color')
-		)
-	);
-	
-	/* Outline: Text Color */
-	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper .wp-block-button.is-style-outline > .wp-block-button__link:not(.has-text-color)' : '.sv100_sv_content_wrapper article .wp-block-button.is-style-outline > .wp-block-button__link:not(.has-text-color)',
-		array_merge(
-			$module->get_setting('bg_color')->get_css_data('color'),
-			$module->get_setting('text_color')->get_css_data('background-color')
 		)
 	);
 
