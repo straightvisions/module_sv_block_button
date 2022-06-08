eea69bac112b9fc0adb9bc59cc48b1fc4ddbf46a
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-06-08 17:07:30 +0200

    Make Selectors compatible with SV100 Premium Setting Controls

--- lib/css/config/general.php
+++ lib/css/config/general.php
@@ -53,11 +53,16 @@
 	
 	// Default: Sizing
 	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper .wp-block-buttons .wp-block-button > .wp-block-button__link' : '.wp-block-buttons .wp-block-button > .wp-block-button__link',
+		is_admin() ? '.editor-styles-wrapper .wp-block-buttons .wp-block-button > .wp-block-button__link' : '.wp-block-buttons .wp-block-button',
 		array_merge(
 			$module->get_setting('font')->get_css_data('font-family'),
 			$module->get_setting('font_size')->get_css_data('font-size','','px'),
-			$module->get_setting('line_height')->get_css_data('line-height'),
+			$module->get_setting('line_height')->get_css_data('line-height')
+		)
+	);
+	echo $_s->build_css(
+		is_admin() ? '.editor-styles-wrapper .wp-block-buttons .wp-block-button > .wp-block-button__link' : '.wp-block-buttons .wp-block-button > .wp-block-button__link',
+		array_merge(
 			$module->get_setting('padding')->get_css_data('padding'),
 			$module->get_setting('border')->get_css_data()
 		)
